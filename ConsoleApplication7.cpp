15ca8b80523867bf478cb4d0e6fba8251972f9c0
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-27 16:56:16 +0300

    Add files via upload

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,19 @@
+﻿// Чертоусова
+// Фибоначчи
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N, i;
+    cin >> N;
+    int A0 = 0, A1 = 1;
+    int A;
+    for (i = 2; i <= N; i++) {
+        A = A1 + A0;
+        A0 = A1;
+        A1 = A;
+    }
+    cout << A;
+}
+
